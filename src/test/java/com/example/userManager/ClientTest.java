80a29fb2b1c2d8ec411fe79dbcda766f2c1c070f
Flavio <flaviostincone711@gmail.com>
2025-12-12 12:36:12 +0100

    12/12/2025
    Implementazione dei profili, anche come dependency default, ma a progetto finito andrà rimosso

--- src/test/java/com/example/userManager/ClientTest.java
+++ src/test/java/com/example/userManager/ClientTest.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClientTest {
+class ClientTest { 
+    
 
 	@Test
 	void contextLoads() {
@@ -18,6 +19,24 @@ class ClientTest {
         String result = "Mario";
         assertEquals("Mario", result);
     }
-  
+    
+    // @Test
+    // void should_CorrectGetClient()
+    // {
+        
+    //     Client client = new Client("Prova", "prova@gmail.com", "1234");
+    //     client.setRole("CLIENT");
+    //     Client saved = repository.save(client);
+
+    //     ClientDTO expected = new ClientDTO(saved.getId(), "Prova", "prova@gmail.com", "1234", "CLIENT");
+    //     ClientDTO actual = service.getClient(saved.getId());
+
+    //     // assertAll(() -> assertEquals(expected.id(), actual.id()),
+    //     //           () -> assertEquals(expected.name(), actual.name()),
+    //     //           () -> assertEquals(expected.email(), actual.email()),
+    //     //           () -> assertEquals(expected.pwd(), actual.pwd()),
+    //     //           () -> assertEquals(expected.role(), actual.role())                
+    //     //         );
+    // }
 
 }
