1a7981a5e96e29689a7b37b4c52ed11a4a0c5881
Flavio <flaviostincone711@gmail.com>
2025-12-15 16:21:34 +0100

    15/12/2025
    Implementazione altri Test

--- src/test/java/com/example/userManager/ClientServiceTest.java
+++ src/test/java/com/example/userManager/ClientServiceTest.java
@@ -3,9 +3,13 @@ package com.example.userManager;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -35,47 +39,90 @@ class ClientServiceTest {
 
         this.clientServiceImpl = new ClientServiceImpl(mockRepository, mockMapper);
     }
-    
-    @Test
-    void should_ReturnNull_WhenIdNotFound()
-    {
-        //given
-        when(mockRepository.findById(1L)).thenReturn(Optional.empty());
 
-        //when
-        ClientDTO actual = clientServiceImpl.getClient(1L);
+    @Nested
+    class getClientsMethod{
+
+        @Test
+        void should_ReturCorrectListClient(){
+        //given
+        List<Client> clientsEntity = Arrays.asList(new Client("Lista1", "Lista1@gmail.com", "1234"), new Client("Lista2", "Lista2@gmail.com", "1234") );
+        List<ClientDTO> expected = Arrays.asList(new ClientDTO("Lista1", "Lista1@gmail.com", "1234", null), new ClientDTO("Lista2", "Lista2@gmail.com", "1234", null) );
+
+        when(mockRepository.findAll()).thenReturn(clientsEntity);
+        when(mockMapper.toListDTO(clientsEntity)).thenReturn(expected);
+
+        //When
+        List<ClientDTO> clients = clientServiceImpl.getClients();
 
         //then
-        assertNull(actual);
-        
+        assertEquals(expected, clients);
 
-        
+        }
     }
 
-    @Test
-    void should_ReturnCorrectClient() {
-        
+    @Nested
+    class getClientMethod{
 
-        //given
-        Client clientEntity = new Client("Test", "Test@gmail.com", "1234");
-        ClientDTO clientDTO = new ClientDTO("Test", "Test@gmail.com", "1234", null);
-        when(mockRepository.findById(1L)).thenReturn(Optional.of(clientEntity));
-        when(mockMapper.toDTO(clientEntity)).thenReturn(clientDTO);
+        @Test
+        void should_ReturnNull_WhenIdNotFound()
+        {
+            //given
+            when(mockRepository.findById(1L)).thenReturn(Optional.empty());
 
-        String expectedName =  "Test";
-        String expectedEmail = "Test@gmail.com";
-        String expectedPwd = "1234";
+            //when
+            ClientDTO actual = clientServiceImpl.getClient(1L);
 
-        //when
-        ClientDTO actual = clientServiceImpl.getClient(1L);
+            //then
+            assertNull(actual);
+            
+        }
 
-        //then
-        assertAll(
-                  () -> assertEquals(expectedName, actual.name()),
-                  () -> assertEquals(expectedEmail, actual.email()),
-                  () -> assertEquals(expectedPwd, actual.pwd())
-        );
+        @Test
+        void should_ReturnCorrectClient() {
+            
+
+            //given
+            Client clientEntity = new Client("Test", "Test@gmail.com", "1234");
+            ClientDTO expected = new ClientDTO("Test", "Test@gmail.com", "1234", null);
+
+            when(mockRepository.findById(1L)).thenReturn(Optional.of(clientEntity));
+            when(mockMapper.toDTO(clientEntity)).thenReturn(expected);
+
+            //when
+            ClientDTO actual = clientServiceImpl.getClient(1L);
+
+            //then
+            assertEquals(expected, actual);
+        }
+
+        //??? Nei Test conviene fare Chiamte Multiple, oppure bisogna fare un @Test per ogni Caso Specifico ???
+        @Disabled 
+        @Test
+        void should_ReturnCorrectClient_when_CalledMultipleTimes(){
+
+            //given
+            Client clientEntity = new Client("Test", "Test@gmail.com", "1234");
+            ClientDTO expectedSecond = new ClientDTO("Test", "Test@gmail.com", "1234", null);
+
+            when(mockRepository.findById(1L))
+                .thenReturn(Optional.empty())
+                .thenReturn(Optional.of(clientEntity));
+
+            when(mockMapper.toDTO(clientEntity)).thenReturn(expectedSecond);
+
+            //when
+            ClientDTO actualFirst = clientServiceImpl.getClient(1L);
+            ClientDTO actualSecond = clientServiceImpl.getClient(1L);
+
+            //then
+            assertAll(
+                () -> assertNull(actualFirst),
+                () -> assertEquals(expectedSecond, actualSecond)
+            );
+
+        }
+
     }
-
 
 }
