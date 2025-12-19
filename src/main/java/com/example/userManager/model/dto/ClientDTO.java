01b57aed6dc21ea0af0383549d996c21ad45e247
Flavio <flaviostincone711@gmail.com>
2025-12-19 12:06:54 +0100

    19/1272025

--- src/main/java/com/example/userManager/model/dto/ClientDTO.java
+++ src/main/java/com/example/userManager/model/dto/ClientDTO.java
@@ -1,6 +1,6 @@
 package com.example.userManager.model.dto;
 
-public record ClientDTO( Long id, String name, String email, String pwd, String role) {
+public record ClientDTO( Long id, @NotBlank String name, String email, String pwd, String role) {
     
     public ClientDTO(String name, String email, String pwd, String role) {
         this( null, name, email, pwd, role);
